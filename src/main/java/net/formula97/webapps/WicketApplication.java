31c4f8948a7a19b72a396f249d1a4c410cfcbe6a
HAJIME Fukuna <f97one@gmail.com>
2019-10-21 11:42:57 +0000

    First commit.

---
+++ src/main/java/net/formula97/webapps/WicketApplication.java
@@ -0,0 +1,33 @@
+package net.formula97.webapps;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.protocol.http.WebApplication;
+
+/**
+ * Application object for your web application.
+ * If you want to run this application without deploying, run the Start class.
+ * 
+ * @see net.formula97.webapps.Start#main(String[])
+ */
+public class WicketApplication extends WebApplication
+{
+	/**
+	 * @see org.apache.wicket.Application#getHomePage()
+	 */
+	@Override
+	public Class<? extends WebPage> getHomePage()
+	{
+		return HomePage.class;
+	}
+
+	/**
+	 * @see org.apache.wicket.Application#init()
+	 */
+	@Override
+	public void init()
+	{
+		super.init();
+
+		// add your configuration here
+	}
+}
