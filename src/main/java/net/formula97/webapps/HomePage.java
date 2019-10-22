2dce38c60e67f110accde9f8812ba5fc553a5701
HAJIME Fukuna <f97one@gmail.com>
2019-10-22 08:40:49 +0000

    ソート機能ありで使うグリッドを張る箇所を追加。

--- src/main/java/net/formula97/webapps/HomePage.java
+++ src/main/java/net/formula97/webapps/HomePage.java
@@ -4,15 +4,20 @@ import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.RepeatingView;
 import org.apache.wicket.markup.repeater.data.DataView;
 import org.apache.wicket.markup.repeater.data.ListDataProvider;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.jetbrains.annotations.NotNull;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
@@ -25,6 +30,10 @@ public class HomePage extends WebPage {
 	protected void onInitialize() {
 		super.onInitialize();
 
+		// ソート機能のないやつ
+		// 各配列のインデックスを一致させることで、データをグリッド的に並べる
+
+		// ヘッダ部分
 		String[] h = {"アイテム1", "アイテム2", "アイテム3"};
 		final List<String> headers = Arrays.asList(h);
 		ListView<String> headerLV = new ListView<String>("tableHeader", headers) {
@@ -35,6 +44,7 @@ public class HomePage extends WebPage {
 		};
 		add(headerLV);
 
+		// 本体部分
 		List<ArrayList<Integer>> internalItems = new ArrayList<>();
 
 		for (int i = 1; i <= 5; i++) {
@@ -45,8 +55,11 @@ public class HomePage extends WebPage {
 			internalItems.add(rowitems);
 		}
 
+		// 本体部分にデータを張る処理
 		ListDataProvider<ArrayList<Integer>> provider = new ListDataProvider<>(internalItems);
 		DataView<ArrayList<Integer>> dataView = new DataView<ArrayList<Integer>>("rows", provider) {
+			private static final long serialVersionUID = -7388444179966849311L;
+
 			@Override
 			protected void populateItem(Item<ArrayList<Integer>> item) {
 				List<Integer> row = item.getModelObject();
@@ -58,6 +71,53 @@ public class HomePage extends WebPage {
 			}
 		};
 		add(dataView);
+
+		// ソート機能のあるやつ
+		List<CityCode> cityCodeList = loadFromCsv("city_code.csv");
+		CityDataProvider cityDataProvider = new CityDataProvider(cityCodeList);
+		DataView<CityCode> cityCodeDataView = new DataView<CityCode>("sortedRow", cityDataProvider) {
+			private static final long serialVersionUID = -804351522780441495L;
+
+			@Override
+			protected void populateItem(Item<CityCode> item) {
+				CityCode cityCode = item.getModelObject();
+
+				item.add(new Label("code", Model.of(cityCode.getCode())));
+				item.add(new Label("pref", Model.of(cityCode.getPref())));
+				item.add(new Label("city", Model.of(cityCode.getCity())));
+			}
+		};
+		cityCodeDataView.setItemsPerPage(10L);
+
+		add(cityCodeDataView);
+		add(new PagingNavigator("navigator", cityCodeDataView));
 	}
+
+	/**
+	 * CSVを型のリストに組み立てなおす。
+	 *
+	 * @param filename 読み込み対象のファイル名
+	 * @return CSVを組み立てなおしたもの
+	 */
+	@NotNull
+	private List<CityCode> loadFromCsv(String filename) {
+		InputStream countriesStream = HomePage.class.getResourceAsStream(filename);
+		Scanner scanner = new Scanner(countriesStream);
+		List<CityCode> countries = new ArrayList<>();
+
+		while(scanner.hasNext()){
+			String curLine = scanner.nextLine();
+			String[] row = curLine.split(",");
+
+			CityCode cityCode = new CityCode(row[0], row[1], row[2]);
+
+			countries.add(cityCode);
+		}
+
+		scanner.close();
+
+		return countries;
+	}
+
 
 }
